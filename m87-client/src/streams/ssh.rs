28567fcff10e0f647f6bc2c2ac57553a510030e6
Nisse Knudsen <nisse.knudsen@gmail.com>
2025-12-11 14:44:30 -0800

    debug prints for analyzing ssh halt

--- m87-client/src/streams/ssh.rs
+++ m87-client/src/streams/ssh.rs
@@ -9,15 +9,19 @@ pub async fn handle_ssh_io(io: QuicIo) {
     let home_dir = dirs::home_dir().unwrap();
     let home_dir = home_dir.to_path_buf();
 
+    tracing::debug!("Creating SSH server config...");
     let config = make_server_config();
+    tracing::debug!("SSH config created, starting run_stream...");
     let handler = M87SshHandler::new(home_dir);
 
     match server::run_stream(config, io, handler).await {
         Ok(running) => {
+            tracing::debug!("SSH handshake complete, session running");
             // Second stage: lifetime of the connection
             if let Err(e) = running.await {
                 tracing::error!("SSH connection closed: {:?}", e);
             }
+            tracing::debug!("SSH session ended normally");
         }
         Err(e) => {
             tracing::error!("SSH handshake aborted: {:?}", e);
